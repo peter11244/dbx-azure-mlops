45a25b3821aeda2b8ccfef14d8a364694110a090
Peter Sach <1846224+peter11244@users.noreply.github.com>
2025-02-03 16:53:29 +0000

    Update Databricks SQL endpoint configuration to set warehouse type and auto-stop settings

--- terraform/3-databricks/sql-warehouses.tf
+++ terraform/3-databricks/sql-warehouses.tf
@@ -1,5 +1,7 @@
 resource "databricks_sql_endpoint" "serverless-main" {
     name = "serverless-main"
     cluster_size = "2X-Small"
+    warehouse_type = "PRO"
+    auto_stop_mins = 10
     enable_serverless_compute = true
 }
